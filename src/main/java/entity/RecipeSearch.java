1d17edf5d183731ad02fd2f165fdcb899684e8cb
Beninix <brandon080808@outlook.com>
2024-11-18 17:48:36 -0500

    Added calories search and cooking time search

--- src/main/java/entity/RecipeSearch.java
+++ src/main/java/entity/RecipeSearch.java
@@ -6,20 +6,39 @@ import java.util.List;
 import java.util.Map;
 
 public class RecipeSearch {
-    public static List<Recipe> dietSearch(List<Recipe> recipeList, Map<String, Boolean> diets){
+    public static List<Recipe> dietSearch(List<Recipe> recipeList, Map<String, Boolean> diets) {
         List<Recipe> output = new ArrayList<Recipe>();
-        for (Recipe r : recipeList){
+        for (Recipe r : recipeList) {
             Boolean invalid = false;
-            for (Map.Entry<String, Boolean> entry : diets.entrySet()){
-                if (entry.getValue() != r.getDiets().get(entry.getKey())){
+            for (Map.Entry<String, Boolean> entry : diets.entrySet()) {
+                if (entry.getValue() != r.getDiets().get(entry.getKey())) {
                     invalid = true;
                     break;
                 }
             }
-            if (!invalid){
+            if (!invalid) {
                 output.add(r);
             }
         }
         return output;
     }
+    public static List<Recipe> caloriesSearch(List<Recipe> recipeList, int maxCalories){
+        List<Recipe> output = new ArrayList<Recipe>();
+        for (Recipe r : recipeList){
+            if(r.getNutrition().getCalories() <= maxCalories){
+                output.add(r);
+            }
+        }
+        return output;
+    }
+    public static List<Recipe> cookingTimeSearch(List<Recipe> recipeList, int maxCookingTimes){
+        List<Recipe> output = new ArrayList<Recipe>();
+        for (Recipe r : recipeList){
+            if(r.getCookingTime() <= maxCookingTimes){
+                output.add(r);
+            }
+        }
+        return output;
+    }
+
 }
