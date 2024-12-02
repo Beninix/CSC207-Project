d5d435c8f7d45194e00a04794020ac3c0a23489f
Beninix <brandon080808@outlook.com>
2024-12-02 00:51:41 -0500

    SearchView wip

--- src/main/java/interface_adapter/search/SearchState.java
+++ src/main/java/interface_adapter/search/SearchState.java
@@ -4,4 +4,9 @@ package interface_adapter.search;
  * The state for the Search View Model.
  */
 public class SearchState {
+    private String searchName = "";
+
+    private String getSearchName(){
+        return searchName;
+    }
 }
